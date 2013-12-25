93dc96390dfdb98203dccec4bd199ba45c81adcd
MichaelWiss <michael.wiss@gmail.com>
2013-12-25 18:44:49 -0500

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ LearnRails::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   # email enabled in production
-config.action_mailer.default_url_options = { :host => ENV["wissmailapp.herokuapp.com"] }
+config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true 
 config.action_mailer.raise_delivery_errors = false 
